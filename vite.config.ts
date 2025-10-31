1a5188f91cff274ac5348ba88f5c8fb501f4bb89
tmanyanov <2737310+darkwebdev@users.noreply.github.com>
2025-10-31 21:06:19 +0100

    fix assets 404 for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  // for GitHub Pages
+  base: '/ak-chars/',
+  // Copy files from the repo-level `public` directory into `dist` during build
+  publicDir: path.resolve(__dirname, 'public'),
   root: path.resolve(__dirname, 'src', 'client'),
   plugins: [
     react(),
@@ -43,5 +47,4 @@ export default defineConfig({
   build: {
     outDir: path.resolve(__dirname, 'dist'),
   },
-  publicDir: path.resolve(__dirname, 'public'),
 });
